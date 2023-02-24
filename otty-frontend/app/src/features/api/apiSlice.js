acea920a26391a7ce5687d9378a45c75566f8efd
3mawy <m.3mawyy@gmail.com>
2023-02-24 22:14:51 +0200

    server and docker config

--- otty-frontend/app/src/features/api/apiSlice.js
+++ otty-frontend/app/src/features/api/apiSlice.js
@@ -3,7 +3,7 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 // import {setCredentials, logOut} from "../auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://localhost:80/api',
+    baseUrl: 'http://localhost:83/api',
     // credentials: 'include',
     prepareHeaders: (headers, {getState}) => {
         const token = getState().auth.token;
